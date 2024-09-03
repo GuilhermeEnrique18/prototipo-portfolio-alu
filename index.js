eeda54173f690901f8b33b9c15151043c5b6a72f
Guilherme Lima <enriqueguilherme1209@gmail.com>
2024-09-03 18:39:30 -0300

    Primeira atualização do portfólio. Adicionando Css, Html e um pouco de JavaScript

---
+++ index.js
@@ -0,0 +1,19 @@
+function mostrarAlertaJs() {
+    alert("Sou proficiente em JavaScript! Posso criar interações dinâmicas para as suas aplicações.");
+}
+
+function mostrarAlertaJava() {
+    alert("Sou proficiente em Java! Posso criar interações dinâmicas para as suas aplicações.");
+}
+
+function mostrarAlertaHtml() {
+    alert("Sou proficiente em Html! Posso criar interações dinâmicas para as suas aplicações.");
+}
+
+function mostrarAlertaCss() {
+    alert("Sou proficiente em Css! Posso criar interações dinâmicas para as suas aplicações.");
+}
+
+function mostrarAlertaGit() {
+    alert("Sou proficiente em Git e Github! Consigo trabalhar em ambientes onde o versionamento é necessário.");
+}
