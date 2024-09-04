22440b2a880cb4d5b508e91b44d0d437f3270fc8
Guilherme Lima <enriqueguilherme1209@gmail.com>
2024-09-04 16:42:59 -0300

    adicao de objetos e atualizacao de links

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-console.log("Ola Mundo!");
+console.log(dados);
 
 function mostrarAlertaJs() {
     alert("Sou proficiente em JavaScript! Posso criar interações dinâmicas para as suas aplicações.");
 }
-
