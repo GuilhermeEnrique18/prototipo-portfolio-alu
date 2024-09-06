9b34f21e547ae7c74676f3757b671fa23c494ff4
Guilherme Lima <enriqueguilherme1209@gmail.com>
2024-09-06 20:59:23 -0300

    Implementando funcionalidade busca

--- app.js
+++ app.js
@@ -1,5 +1,50 @@
-console.log(dados);
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    campoPesquisa = campoPesquisa.toLowerCase();
+    //console.log(campoPesquisa);
+    let resultados = "";
+    let linguagem = "";
+    let descricao = "";
+   
+    for(let dado of dados) {
+      linguagem = dado.linguagem.toLowerCase();
+      descricao = dado.descricao.toLowerCase();
+      
+      if(linguagem.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+        
+        resultados += `
 
-function mostrarAlertaJs() {
-    alert("Sou proficiente em JavaScript! Posso criar interações dinâmicas para as suas aplicações.");
-}
+        <div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank">${dado.linguagem}</a>
+            </h2>
+
+                <p class="descricao-meta">${dado.descricao}</p>
+
+                <a href=${dado.link} target="_blank"> Projetos em Js </a> 
+        </div>
+`
+          }
+          
+          if(campoPesquisa == ""){ 
+            section.innerHTML = "digite novamente, tente JavaScript ou Git";
+            return
+          } 
+          
+      }
+
+      section.innerHTML = resultados;
+  }
+
+
+        // let delay = 0;
+      // resultados.split('</div>').forEach(resultado => {
+      //   setTimeout(() => {
+      //     section.insertAdjacentHTML('beforeend', resultado + '</div>');
+      //     // Adicione uma classe CSS para aplicar um efeito de animação
+      //     let novoResultado = section.lastElementChild;
+      //     novoResultado.classList.add('mostrar');
+      //   }, delay);
+      //   delay += 500; // Ajuste o valor para controlar a velocidade
+      // } );
