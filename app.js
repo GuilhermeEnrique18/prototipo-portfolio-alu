a5e394ca67f9c282cebddf06b300d3bacd65e650
Guilherme Lima <enriqueguilherme1209@gmail.com>
2024-09-06 22:06:46 -0300

    Deploy

--- app.js
+++ app.js
@@ -1,50 +1,59 @@
 function pesquisar() {
-    let section = document.getElementById("resultados-pesquisa");
-    let campoPesquisa = document.getElementById("campo-pesquisa").value;
-    campoPesquisa = campoPesquisa.toLowerCase();
-    //console.log(campoPesquisa);
-    let resultados = "";
-    let linguagem = "";
-    let descricao = "";
-   
-    for(let dado of dados) {
-      linguagem = dado.linguagem.toLowerCase();
-      descricao = dado.descricao.toLowerCase();
-      
-      if(linguagem.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
-        
-        resultados += `
+  let section = document.getElementById("resultados-pesquisa");
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+  campoPesquisa = campoPesquisa.toLowerCase();
+  //console.log(campoPesquisa);
+  let resultados = "";
+  let linguagem = "";
+  let descricao = "";
+  let tags = "";
+
+  for (let dado of dados) {
+    linguagem = dado.linguagem.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+    
+
+    if (linguagem.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+
+      resultados += `
 
         <div class="item-resultado">
             <h2>
-                <a href="#" target="_blank">${dado.linguagem}</a>
+                <a href="${dado.link}" target="_blank">${dado.linguagem}</a>
             </h2>
 
                 <p class="descricao-meta">${dado.descricao}</p>
 
-                <a href=${dado.link} target="_blank"> Projetos em Js </a> 
+                <a href=${dado.link} target="_blank"> Projetos em ${dado.linguagem} </a> 
         </div>
 `
-          }
-          
-          if(campoPesquisa == ""){ 
-            section.innerHTML = "digite novamente, tente JavaScript ou Git";
-            return
-          } 
-          
-      }
+    }
 
-      section.innerHTML = resultados;
+    if (campoPesquisa == "") {
+      section.innerHTML = "digite novamente, tente JavaScript ou Git";
+      return
+    }
+
+  }
+
+  if(!resultados ){
+    resultados = "<p>Nada foi encontrado</p>"
+
   }
 
 
-        // let delay = 0;
-      // resultados.split('</div>').forEach(resultado => {
-      //   setTimeout(() => {
-      //     section.insertAdjacentHTML('beforeend', resultado + '</div>');
-      //     // Adicione uma classe CSS para aplicar um efeito de animação
-      //     let novoResultado = section.lastElementChild;
-      //     novoResultado.classList.add('mostrar');
-      //   }, delay);
-      //   delay += 500; // Ajuste o valor para controlar a velocidade
-      // } );
+  section.innerHTML = resultados;
+}
+
+
+// let delay = 0;
+// resultados.split('</div>').forEach(resultado => {
+//   setTimeout(() => {
+//     section.insertAdjacentHTML('beforeend', resultado + '</div>');
+//     // Adicione uma classe CSS para aplicar um efeito de animação
+//     let novoResultado = section.lastElementChild;
+//     novoResultado.classList.add('mostrar');
+//   }, delay);
+//   delay += 500; // Ajuste o valor para controlar a velocidade
+// } );
--- database.js
+++ database.js
@@ -3,19 +3,20 @@ let dados = [
         linguagem: "JavaScript",
         descricao: "Proficiência em JavaScript para desenvolvimento front-end. Conhecimento em DOM, manipulação de eventos, AJAX e frameworks como React. Criação de interfaces dinâmicas e interativas.",
         link: "https://github.com/GuilhermeEnrique18/prototipo-portfolio-alu",
-    
+        tags: "Front end Front"
+
     },
     {
         linguagem: "Java",
         descricao: "Possuo um sólido conhecimento nos fundamentos da linguagem Java, incluindo orientação a objetos, herança, polimorfismo e encapsulamento. Sou proficiente em estruturas de dados como arrays e listas, e emalgoritmos básicos de ordenação e busca. Domino o uso de classes e métodos.",
         link: "https://github.com/GuilhermeEnrique18/StartHub2",
-    
+        tags: "Spring"
     },
     {
         linguagem: "Git e GitHub",
         descricao: "Experiência em controle de versão utilizando Git e GitHub. Dominando comandos como clone, branch, merge e pull request. Criação e manutenção de repositórios, colaboração emprojetos e resolução de conflitos.",
         link: "https://github.com/GuilhermeEnrique18?tab=repositories",
-    
+        tags: "Versionamento"
     },
 ];
 
